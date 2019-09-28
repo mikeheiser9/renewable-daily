539e028754025e5e2940ebe33f90125c1c18eae6
mikeheiser9 <mikeheiser9@gmail.com>
2019-09-28 16:31:07 -0400

    adding again

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -79,7 +79,6 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/zoning.html"));
   });
 
-
   // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
